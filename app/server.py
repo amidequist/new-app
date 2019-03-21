c59b5646c37b439faa21208d702c1e34b839a601
asiedubrempong <kaybrempong@gmail.com>
2019-03-21 02:30:26 +0000

    update ui

--- app/server.py
+++ app/server.py
@@ -54,7 +54,12 @@ async def analyze(request):
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    top_probs, top_classes = prediction_info[2].topk(5)
+    # if probability is below 0.7 then it's not an image
+    if top_probs[0].item() < 0.7:
+        return JSONResponse({'result': 'Seems like the image is not an image of cocoa})
+    else:
+        return JSONResponse({'result': str(prediction)})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
